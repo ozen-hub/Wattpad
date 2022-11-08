abaa5a247f89e3c8cfae4b120899aba9388c562d
ozen-hub <info.ozenhub@gmail.com>
2022-11-08 12:22:39 +0530

    11

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -1,4 +1,47 @@
 package com.seekerscloud.pos.controller;
 
+import com.seekerscloud.pos.db.Database;
+import com.seekerscloud.pos.model.Customer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CustomerFormController {
+    public AnchorPane customerFormContext;
+    public TextField txtId;
+    public TextField txtSalary;
+    public TextField txtAddress;
+    public TextField txtName;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashBoardForm", "Dashboard");
+    }
+
+    private void setUi(String location,String title) throws IOException {
+        Stage window = (Stage)customerFormContext.getScene().getWindow();
+        window.setTitle(title);
+        window.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml")))
+        );
+    }
+
+    public void saveUpdateOnAction(ActionEvent actionEvent) {
+        Customer customer = new Customer(
+                txtId.getText(),
+                txtName.getText(),
+                txtAddress.getText(),
+                Double.parseDouble(txtSalary.getText())
+        );
+        if (Database.customerTable.add(customer)){
+            new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved!").show();
+        }else{
+            new Alert(Alert.AlertType.CONFIRMATION, "Try Again").show();
+        }
+    }
 }
--- src/com/seekerscloud/pos/db/Database.java
+++ src/com/seekerscloud/pos/db/Database.java
@@ -1,9 +1,11 @@
 package com.seekerscloud.pos.db;
 
+import com.seekerscloud.pos.model.Customer;
 import com.seekerscloud.pos.model.User;
 
 import java.util.ArrayList;
 
 public class Database {
     public static ArrayList<User> userTable = new ArrayList<User>();
+    public static ArrayList<Customer> customerTable = new ArrayList<Customer>();
 }
