fe8703b90e5a237be8242dd4d9f2144c7a0b2ae3
ozen-hub <info.ozenhub@gmail.com>
2022-11-08 11:31:13 +0530

    7

--- src/com/seekerscloud/pos/controller/DashBoardFormController.java
+++ src/com/seekerscloud/pos/controller/DashBoardFormController.java
@@ -1,4 +1,26 @@
 package com.seekerscloud.pos.controller;
 
+import javafx.scene.control.Label;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DashBoardFormController {
+    public Label lblDate;
+    public Label lblTime;
+
+    public void initialize(){
+        setDateAndTime();
+    }
+
+    private void setDateAndTime() {
+        // set Date
+  /*      Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = dateFormat.format(date);
+        lblDate.setText(formattedDate);*/
+        lblDate.setText(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+        //===================
+    }
+
 }
