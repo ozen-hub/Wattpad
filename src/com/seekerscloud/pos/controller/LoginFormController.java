126604bdd35039ccade9d88ffb4859e84e89c807
ozen-hub <info.ozenhub@gmail.com>
2022-11-08 09:36:53 +0530

    1

--- src/com/seekerscloud/pos/controller/LoginFormController.java
+++ src/com/seekerscloud/pos/controller/LoginFormController.java
@@ -1,4 +1,25 @@
 package com.seekerscloud.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane loginFormContext;
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignUpForm");
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage window = (Stage)loginFormContext.getScene().getWindow();
+        window.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml")))
+        );
+    }
 }
---
+++ src/com/seekerscloud/pos/db/Database.java
@@ -0,0 +1,9 @@
+package com.seekerscloud.pos.db;
+
+import com.seekerscloud.pos.model.User;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<User> userTable = new ArrayList<User>();
+}
