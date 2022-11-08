13b729a375245f13110762016c5c8fb6621e733d
ozen-hub <info.ozenhub@gmail.com>
2022-11-08 11:56:15 +0530

    9

--- src/com/seekerscloud/pos/controller/DashBoardFormController.java
+++ src/com/seekerscloud/pos/controller/DashBoardFormController.java
@@ -3,9 +3,15 @@ package com.seekerscloud.pos.controller;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -16,6 +22,7 @@ import java.util.Date;
 public class DashBoardFormController {
     public Label lblDate;
     public Label lblTime;
+    public AnchorPane dashboardContext;
 
     public void initialize() {
         setDateAndTime();
@@ -40,6 +47,30 @@ public class DashBoardFormController {
         }),new KeyFrame(Duration.seconds(1)));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
+    }
+
+    public void openCustomerFormOnAction(MouseEvent mouseEvent) throws IOException {
+        setUi("CustomerForm","Customer Form");
+    }
+
+    public void openOrderDetailsOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void openPlaceOrderFormOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void openProductManagementOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void openStatisticsOnAction(MouseEvent mouseEvent) {
+    }
+
+    private void setUi(String location,String title) throws IOException {
+        Stage window = (Stage)dashboardContext.getScene().getWindow();
+        window.setTitle(title);
+        window.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml")))
+        );
     }
 
 }
