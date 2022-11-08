0ba4a1b9b84280f9734302ea031dc1d0a8d14225
ozen-hub <info.ozenhub@gmail.com>
2022-11-08 10:11:07 +0530

    3

--- src/com/seekerscloud/pos/controller/SignUpFormController.java
+++ src/com/seekerscloud/pos/controller/SignUpFormController.java
@@ -5,7 +5,13 @@ import com.jfoenix.controls.JFXTextField;
 import com.seekerscloud.pos.db.Database;
 import com.seekerscloud.pos.model.User;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignUpFormController {
     public JFXTextField txtEmail;
@@ -13,8 +19,9 @@ public class SignUpFormController {
     public JFXTextField txtFullName;
     public JFXTextField txtContact;
     public JFXPasswordField txtPassword;
+    public AnchorPane signupFormContext;
 
-    public void signUpOnAction(ActionEvent actionEvent) {
+    public void signUpOnAction(ActionEvent actionEvent) throws InterruptedException, IOException {
         // check password match?
         String realPwd=txtPassword.getText().trim(); // trim() => __N_ after trim() => N
         String matchPwd=txtRePassword.getText().trim();
@@ -28,6 +35,8 @@ public class SignUpFormController {
         if(saveUser(u)) {
             new Alert(Alert.AlertType.CONFIRMATION, "User Registered!").show();
             clearFields();
+            Thread.sleep(2000); // wait 2 seconds
+            setUi("DashBoardForm");
         } else {
             new Alert(Alert.AlertType.WARNING, "Try Again!").show();
         }
@@ -37,5 +46,16 @@ public class SignUpFormController {
     }
     private boolean saveUser(User u){
         return Database.userTable.add(u);// inbuilt method ==> java.util
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage window = (Stage)signupFormContext.getScene().getWindow();
+        window.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml")))
+        );
+    }
+
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
     }
 }
