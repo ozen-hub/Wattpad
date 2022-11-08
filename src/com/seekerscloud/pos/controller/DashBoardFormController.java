601ee6bae8c2682626afeda893d669a11a73c337
ozen-hub <info.ozenhub@gmail.com>
2022-11-08 11:43:09 +0530

    8

--- src/com/seekerscloud/pos/controller/DashBoardFormController.java
+++ src/com/seekerscloud/pos/controller/DashBoardFormController.java
@@ -1,26 +1,45 @@
 package com.seekerscloud.pos.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DashBoardFormController {
     public Label lblDate;
     public Label lblTime;
 
-    public void initialize(){
+    public void initialize() {
         setDateAndTime();
     }
 
     private void setDateAndTime() {
         // set Date
-  /*      Date date = new Date();
+        /*Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String formattedDate = dateFormat.format(date);
         lblDate.setText(formattedDate);*/
         lblDate.setText(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
         //===================
+        //set Time
+        final DateFormat format = DateFormat.getDateInstance();
+        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, e->{
+           /* Calendar cal = Calendar.getInstance();
+            lblTime.setText(format.format(cal.getTime()));*/
+            lblTime.setText(LocalDateTime.now().format(
+                    DateTimeFormatter.ofPattern("HH:mm:ss")
+            ));
+        }),new KeyFrame(Duration.seconds(1)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
 }
