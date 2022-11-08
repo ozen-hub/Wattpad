51ce01246718fb47eb3e18522c2570110aa942dc
ozen-hub <info.ozenhub@gmail.com>
2022-11-08 12:36:36 +0530

    12

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -2,15 +2,19 @@ package com.seekerscloud.pos.controller;
 
 import com.seekerscloud.pos.db.Database;
 import com.seekerscloud.pos.model.Customer;
+import com.seekerscloud.pos.view.tm.CustomerTM;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CustomerFormController {
     public AnchorPane customerFormContext;
@@ -18,6 +22,23 @@ public class CustomerFormController {
     public TextField txtSalary;
     public TextField txtAddress;
     public TextField txtName;
+    public TableView<CustomerTM> tblCustomer;
+    public TableColumn colCustomerId;
+    public TableColumn colCustomerName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableColumn colOption;
+
+    public void initialize(){
+        setTableData();
+        //==============
+        colCustomerId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colCustomerName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
+        //==============
+    }
 
     public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
         setUi("DashBoardForm", "Dashboard");
@@ -40,8 +61,22 @@ public class CustomerFormController {
         );
         if (Database.customerTable.add(customer)){
             new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved!").show();
+            setTableData();
         }else{
             new Alert(Alert.AlertType.CONFIRMATION, "Try Again").show();
         }
     }
+
+    private void setTableData(){
+        ArrayList<Customer> customerList=Database.customerTable;
+        ObservableList<CustomerTM> obList= FXCollections.observableArrayList();
+        for (Customer c:customerList
+             ) {
+            Button btn= new Button("Delete");
+            CustomerTM tm = new CustomerTM(c.getId(),c.getName(),c.getAddress(),c.getSalary(),btn);
+            obList.add(tm);
+        }
+        tblCustomer.setItems(obList);
+    }
+
 }
