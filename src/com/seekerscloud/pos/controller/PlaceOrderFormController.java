330c99681d38891f53c565cdef1ae8f4ed4148c6
ozen-hub <info.ozenhub@gmail.com>
2022-11-10 11:22:59 +0530

    19

--- src/com/seekerscloud/pos/controller/PlaceOrderFormController.java
+++ src/com/seekerscloud/pos/controller/PlaceOrderFormController.java
@@ -9,19 +9,65 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class PlaceOrderFormController {
     public AnchorPane placeOrderContext;
     public ComboBox<String> cmbCustomerCodes;
     public ComboBox<String> cmbItemCodes;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+    public TextField txtDescription;
+    public TextField txtUnitPrice;
+    public TextField txtQtyOnHand;
 
     public void initialize() {
         loadCustomerIds();
         loadItemCodes();
+
+        //==============Listeners============
+            cmbCustomerCodes.getSelectionModel()
+                    .selectedItemProperty()
+                    .addListener((observable, oldValue, newValue) -> {
+                        setCustomerData(newValue);
+                    });
+        cmbItemCodes.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    setProductData(newValue);
+                });
+        //==============Listeners============
+
+    }
+
+    private void setProductData(String code) {
+        Product product = Database.productTable.stream().filter(e -> e.getCode().equals(code))
+                .findFirst().orElse(null);
+        if (product!=null){
+            txtDescription.setText(product.getDescription());
+            txtUnitPrice.setText(String.valueOf(product.getUnitPrice()));
+            txtQtyOnHand.setText(String.valueOf(product.getQtyOnHand()));
+        }
+    }
+
+    private void setCustomerData(String id) {
+        Stream<Customer> customerList =
+                Database.customerTable.stream().filter(e -> e.getId().equals(id));
+        Optional<Customer> first = customerList.findFirst();
+        if (first.isPresent()){
+            Customer customer = first.get();
+            txtName.setText(customer.getName());
+            txtAddress.setText(customer.getAddress());
+            txtSalary.setText(String.valueOf(customer.getSalary()));
+        }
+
     }
 
     private void loadCustomerIds() {
