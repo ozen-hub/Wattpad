aaca947cb7756d0c705ec8b24e7234188f9d2d57
ozen-hub <info.ozenhub@gmail.com>
2022-11-10 10:55:25 +0530

    17

--- src/com/seekerscloud/pos/db/Database.java
+++ src/com/seekerscloud/pos/db/Database.java
@@ -10,4 +10,17 @@ public class Database {
     public static ArrayList<User> userTable = new ArrayList<User>();
     public static ArrayList<Customer> customerTable = new ArrayList<Customer>();
     public static ArrayList<Product> productTable = new ArrayList<Product>();
+
+    static {
+        customerTable.add(new Customer("C-001","Jayantha","Colombo",450000));
+        customerTable.add(new Customer("C-002","Kamal","Kalutara",250000));
+        customerTable.add(new Customer("C-003","Wasantha","Panadura",850000));
+
+        productTable.add(new Product("D-001","Description 1",25,4500));
+        productTable.add(new Product("D-002","Description 2",90,75000));
+        productTable.add(new Product("D-003","Description 3",820,420));
+        productTable.add(new Product("D-004","Description 4",45,480));
+        productTable.add(new Product("D-005","Description 5",12,650));
+
+    }
 }
